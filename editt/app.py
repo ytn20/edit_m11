c29fdd59c9c02df5c4a8125b00c35df0a218bdbf
ytn20 <100486461+ytn20@users.noreply.github.com>
2022-07-21 01:26:59 -0400

    Add files via upload

---
+++ editt/app.py
@@ -0,0 +1,19 @@
+# Import Dependencies
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello world'
+
+export FLASK_APP=app.py
+
+set FLASK_APP=app.py
+
+flask run
+
+
+
+
+
